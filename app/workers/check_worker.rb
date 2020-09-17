9d1d6ab75d396771e642e8c790dd0dd0a4827ea3
Vadesto <confusedyeen@gmail.com>
2020-09-17 15:52:18 +0700

    Fully implemented and working (for now) ssl checker

--- app/workers/check_worker.rb
+++ app/workers/check_worker.rb
@@ -7,21 +7,28 @@ class CheckWorker
   def perform(link_id)
     link = Link.find(link_id)
     uri = URI(link.url)
-    response = Net::HTTP.start(uri.host, uri.port, :use_ssl => true)
-    cert = response.peer_cert || nil
+    
+    begin
+      response = Net::HTTP.start(uri.host, uri.port, :use_ssl => true)
+      cert = response.peer_cert || nil
 
-    week = 7 * 86400
+      week = 7 * 86400
 
-    if cert == nil
-      link.status = :broken
-    elsif Time.now > cert.not_after
-      link.status = :expired
-    elsif Time.now + week > cert.not_after
-      link.status = :expires_in_one_week
-    elsif Time.now + (week*2) > cert.not_after
-      link.status = :expires_in_two_weeks
-    else
-      link.status = :good
+      if cert == nil
+        link.status = :broken
+      elsif Time.now + week > cert.not_after
+        link.status = :expires_in_one_week
+      elsif Time.now + (week*2) > cert.not_after
+        link.status = :expires_in_two_weeks
+      else
+        link.status = :good
+      end
+    rescue OpenSSL::SSL::SSLError => e
+      if e.message.include? "expired"
+        link.status = :expired
+      else
+        link.status = :broken
+      end
     end
 
     link.save
